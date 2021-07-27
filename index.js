5305463d7d2e49eaeffa58ac66097423af9ddee7
Sebastian Hancock <sebastian.hancock@tum.de>
2021-07-27 13:06:09 +0200

    update

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ mapboxgl.accessToken =
     container: "map", // container ID
     //style: 'mapbox://styles/mapbox/light-v10', //?optimize=true
     style: 'mapbox://styles/mapbox/light-v10', 
-    center: [-71.4, 19.1], // starting position [lng, lat]
-    zoom: 4,
+    center: [-77.4, 18.16], // starting position [lng, lat]
+    zoom: 7,
     //pitch: 55
   });
+
+  var firstSymbolId;
 
 
   map.on("load", function () {
@@ -32,55 +34,121 @@ var colors = ['#edf8fb', '#b2e2e2','#66c2a4','#2ca25f', '#006d2c' ]
 
 function addSub() {
 
+  var layers = map.getStyle().layers;
+    //console.log(layers);
+    // Find the index of the first symbol layer in the map style
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+            firstSymbolId = layers[i].id;
+            break;
+        }
+    }
+
   var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-  d3.json('./cable-geo.json').then(function(d) {
-
+  
+  d3.json('./jamaica.geojson').then(function(d) {
 
     console.log(d);
-
+    
     map.addSource('cable', {
       type: 'geojson',
       data: d
     })
 
-    map.addLayer({
-      'id': 'sub',
-      'type': 'line',
-      'source': 'cable',
-      'paint': {
-        
-        'line-color': ['get', 'color'],
-        'line-width': 3
+    
+
+    var clipped = turf.mask(d)
+    console.log(clipped)
+
+    
+
+
+  const hex5 = 'https://sebastian-ch.github.io/sidsDataTest/data/t5/{z}/{x}/{y}.pbf';
+
+
+  map.addSource('hex5', {
+    'type': 'vector',
+    'promoteId': 'hexid',
+    'tiles': [
+      //otherhex
+      hex5,
       
-      }
-    })
+    ],
+    //'minzoom': 3,
+    'maxzoom': 12
   })
 
+  var colorRamp1 = ['#edf8fb', '#b2e2e2', '#66c2a4', '#2ca25f', '#006d2c']
 
-  d3.json('./landing-point-geo.json').then(function(data) {
-    map.addSource('dots', {
-      type: 'geojson',
-      data: data
-    })
+  map.addLayer({
+    'id': 'hex5',
+    'type': 'fill',
+    'source': 'hex5',
+    'source-layer': 'hex5_3857',
+    'layout': {},
+    'paint': {
+        'fill-color':  [
+          'interpolate',
+          ['linear'],
+          ['get', '1a2'],
+          30000, '#edf8fb',
+          36000, '#b2e2e2',
+          900000, '#66c2a4',
+          2500000, '#2ca25f',
+          47000000, '#006d2c',
+          ],
+        'fill-opacity': 0.8
+    }
+},firstSymbolId);
 
-    map.addLayer({
-      id: 'dots',
-      source: 'dots',
-      type: 'circle',
-      paint: {
-        'circle-color': 'red',
-        'circle-radius': 8
-      }
-    })
+map.addLayer({
+  'id': 'mask',
+  'type': 'fill',
+  'source': {
+      'type': 'geojson',
+      'data': clipped
+  },
+  'layout': {},
+  'paint': {
+      'fill-color': '#a9a9a9',
+      'fill-opacity': 1
+  }
+},firstSymbolId);
+
+
+map.addLayer({
+  'id': 'sub',
+  'type': 'line',
+  'source': 'cable',
+  'paint': {
+    
+    'line-color': '#838383',
+    'line-width': 3
+  
+  }
+},firstSymbolId)
+
+
+
+
+
+      
+
+
+
+
+
 
 
   })
 
-
-
-
+ 
 }
+
+
+
+
+
 
 /*function add1km() {
   console.log('hi')
